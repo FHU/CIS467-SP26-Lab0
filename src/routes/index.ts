7ee3281bf237a57852dea158849be6b4d38294ee
Dillon <157512237+DillonDaughrity@users.noreply.github.com>
2026-02-11 12:35:10 -0600

    Added Routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
 import healthRoutes from "./health.routes.js";
 import tasksRoutes from "./tasks.routes.js";
+import usersRoutes from "./users.routes.js";
+import speakersRoutes from "./speakers.routes.js";
+import feedbacksRoutes from "./feedbacks.routes.js";
+import chapelSessionsRoutes from "./chapel-sessions.routes.js";
 
 const router = Router();
 
 router.use("/health", healthRoutes);
 router.use("/tasks", tasksRoutes);
-
+router.use("/users", usersRoutes);
+router.use("/speakers", speakersRoutes);
+router.use("/feedbacks", feedbacksRoutes);
+router.use("/chapel-sessions", chapelSessionsRoutes);
 
 export default router;
